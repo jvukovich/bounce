f3953abafc35002a9382264ef21b6cc7f4cb810e
Tim Macfarlane <timmacfarlane@gmail.com>
2010-09-05 23:10:22 +0100

    classes in their own files

---
+++ Bounce.VisualStudio.Tests/VisualStudioCSharpProjectFileLoaderTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Bounce.VisualStudio.Tests {
+    [TestFixture]
+    public class VisualStudioCSharpProjectFileLoaderTest {
+        [Test]
+        public void ShouldLoadLibraryFile () {
+            var loader = new VisualStudioCSharpProjectFileLoader();
+            string path = @"TestFiles\Bounce.csproj";
+            var details = loader.LoadProject(path, "");
+
+            Assert.That(details.OutputFile, Is.EqualTo(@"bin\Debug\Bounce.dll"));
+        }
+
+        [Test]
+        public void ShouldLoadLibraryFileWithConfiguration () {
+            var loader = new VisualStudioCSharpProjectFileLoader();
+            string path = @"TestFiles\Bounce.csproj";
+            var details = loader.LoadProject(path, "Release");
+
+            Assert.That(details.OutputFile, Is.EqualTo(@"bin\Release\Bounce.dll"));
+        }
+    }
+}
---
+++ Bounce.VisualStudio.Tests/VisualStudioSolutionFileReaderTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+
+namespace Bounce.VisualStudio.Tests {
+    [TestFixture]
+    public class VisualStudioSolutionFileReaderTest {
+        [Test]
+        public void ShouldReadSolutionFile() {
+            var path = @"solutionPath\mysolution.sln";
+
+            var loader = new Mock<IVisualStudioSolutionFileLoader>();
+            loader.Setup(l => l.LoadVisualStudioSolution(path)).Returns(new VisualStudioSolutionFileDetails
+            {VisualStudioProjects = new[] {
+                new VisualStudioSolutionProject {Name = "Stuff", Path = "StuffPath.csproj"},
+                new VisualStudioSolutionProject {Name = "Other", Path = "OtherPath.csproj"},
+            }});
+
+            var projectLoader = new Mock<IVisualStudioProjectFileLoader>();
+            projectLoader
+                .Setup(p => p.LoadProject(@"solutionPath\StuffPath.csproj", "configuration"))
+                .Returns(new VisualStudioCSharpProjectFileDetails { OutputFile = "stuff.dll" });
+            projectLoader
+                .Setup(p => p.LoadProject(@"solutionPath\OtherPath.csproj", "configuration"))
+                .Returns(new VisualStudioCSharpProjectFileDetails { OutputFile = "other.dll" });
+
+            var reader = new VisualStudioSolutionFileReader(loader.Object, projectLoader.Object);
+
+            var solution = reader.ReadSolution(path, "configuration");
+
+            IEnumerable<VisualStudioCSharpProjectFileDetails> projects = solution.Projects;
+
+            Assert.That(projects.Count(), Is.EqualTo(2));
+            Assert.That(projects.ElementAt(0).OutputFile, Is.EqualTo("stuff.dll"));
+            Assert.That(projects.ElementAt(1).OutputFile, Is.EqualTo("other.dll"));
+        }
+    }
+}
