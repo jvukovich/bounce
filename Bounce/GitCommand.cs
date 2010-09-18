d6d2ff963d63ebbb5faeebb6505c571785c06043
Tim Macfarlane <timmacfarlane@gmail.com>
2010-09-18 01:50:46 +0100

    Fixed git command execution problem

--- Bounce/GitCommand.cs
+++ Bounce/GitCommand.cs
@@ -9,11 +9,17 @@ namespace Bounce.Framework {
         }
 
         public void Pull() {
-            ShellCommandExecutor.ExecuteAndExpectSuccess("git", "pull");
+            Console.WriteLine("pulling git repo");
+            var output = ShellCommandExecutor.Execute("cmd", @"/C git pull");
+            Console.WriteLine(output.ExitCode);
+            Console.WriteLine(output.ErrorAndOutput);
         }
 
         public void Clone(string repo, string directory) {
-            ShellCommandExecutor.ExecuteAndExpectSuccess("git", String.Format(@"clone {0} ""{1}""", repo, directory));
+            Console.WriteLine("cloning git repo: {0}, into: {1}", repo, directory);
+            var output = ShellCommandExecutor.Execute("cmd", String.Format(@"/C git clone {0} ""{1}""", repo, directory));
+            Console.WriteLine(output.ExitCode);
+            Console.WriteLine(output.ErrorAndOutput);
         }
     }
 }
--- TestBounceAssembly/BuildTargets.cs
+++ TestBounceAssembly/BuildTargets.cs
@@ -5,18 +5,36 @@ namespace TestBounceAssembly {
     public class BuildTargets {
         [Targets]
         public static object Targets (IParameters parameters) {
-            var solution = new VisualStudioSolution { SolutionPath = parameters.Default("sln", @"C:\Users\Public\Documents\Development\BigSolution\BigSolution.sln") };
+            var git = new GitWorkingTree {
+                Repository = @"C:\Users\Public\Documents\Development\BigSolution.git".V(),
+                Directory = "one".V()
+            };
+            var solution = new VisualStudioSolution {
+                SolutionPath = git["BigSolution.sln".V()]
+            };
             var webProject = solution.Projects[parameters.Default("proj", "BigSolution")];
             var serviceName = parameters.Default("svc", "BigWindowsService");
             var service = solution.Projects[serviceName];
 
             return new {
-                WebSite = new Iis7WebSite {Path = webProject.Directory, Name = "BigWebSite".V(), Port = 5001.V()},
+                WebSite = new Iis7WebSite {
+                    Path = webProject.Directory,
+                    Name = "BigWebSite".V(),
+                    Port = 5001.V()
+                },
                 Tests = new NUnitTests {
                     DllPaths = solution.Projects.Select(p => p.OutputFile)
                 },
-                Service = new WindowsService {BinaryPath = service.OutputFile, Name = serviceName, DisplayName = "Big Windows Service".V(), Description = "a big windows service demonstrating the bounce build framework".V()},
-                Zip = new ZipFile {Directory = webProject.Directory, ZipFileName = "web.zip".V()},
+                Service = new WindowsService {
+                    BinaryPath = service.OutputFile,
+                    Name = serviceName,
+                    DisplayName = "Big Windows Service".V(),
+                    Description = "a big windows service demonstrating the bounce build framework".V()
+                },
+                Zip = new ZipFile {
+                    Directory = webProject.Directory,
+                    ZipFileName = "web.zip".V()
+                },
             };
         }
     }
