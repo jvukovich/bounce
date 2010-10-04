6d9f1cf4e9d022aa5379f402fa770df52db8a7ba
Tim Macfarlane <timmacfarlane@gmail.com>
2010-10-04 19:17:39 +0100

    outputting in standard error format

--- Bounce.Framework/MSBuildLogger.cs
+++ Bounce.Framework/MSBuildLogger.cs
@@ -7,19 +7,22 @@ namespace Bounce.Framework {
 
         public void Initialize(IEventSource eventSource) {
             eventSource.ErrorRaised += eventSource_ErrorRaised;
-            Output = new StreamWriter(new FileStream("msbuild_output.txt", FileMode.Create));
+//            Output = new StreamWriter(new FileStream("msbuild_output.txt", FileMode.Create));
+            Output = System.Console.Out;
         }
 
         void eventSource_ErrorRaised(object sender, BuildErrorEventArgs e) {
-            Output.WriteLine("code: " + e.Code);
-            Output.WriteLine("colnumber: " + e.ColumnNumber);
-            Output.WriteLine("endcolnumber: " + e.EndColumnNumber);
-            Output.WriteLine("endlinenumber: " + e.EndLineNumber);
-            Output.WriteLine("file: " + e.File);
-            Output.WriteLine("linenumber: " + e.LineNumber);
-            Output.WriteLine("message: " + e.Message);
-            Output.WriteLine("subcat: " + e.Subcategory);
-            Output.WriteLine("timestamp: " + e.Timestamp);
+//            Output.WriteLine("code: " + e.Code);
+//            Output.WriteLine("colnumber: " + e.ColumnNumber);
+//            Output.WriteLine("endcolnumber: " + e.EndColumnNumber);
+//            Output.WriteLine("endlinenumber: " + e.EndLineNumber);
+//            Output.WriteLine("file: " + e.File);
+//            Output.WriteLine("linenumber: " + e.LineNumber);
+//            Output.WriteLine("message: " + e.Message);
+//            Output.WriteLine("subcat: " + e.Subcategory);
+//            Output.WriteLine("timestamp: " + e.Timestamp);
+
+            Output.WriteLine("{0}({1},{2}): error {3}: {4}", e.File, e.LineNumber, e.ColumnNumber, e.Code, e.Message);
         }
 
         public void Shutdown() {
