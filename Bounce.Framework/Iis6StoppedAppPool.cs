4831532eddc528c68cc1af2ac3d201320de9c169
Tim Macfarlane <timmacfarlane@gmail.com>
2011-02-14 13:24:02 +0000

    oops, forgot to add these files!

---
+++ Bounce.Framework/Iis6StoppedAppPool.cs
@@ -0,0 +1,15 @@
+﻿namespace Bounce.Framework
+{
+    public class Iis6StoppedAppPool : Iis6Task
+    {
+        [Dependency]
+        public Future<string> Name;
+
+        public override void Build() {
+            IisAppPool appPool = Iis.FindAppPoolByName(Name.Value);
+            if (appPool != null) {
+                appPool.Stop();
+            }
+        }
+    }
+}
---
+++ Bounce.Framework/Iis6StoppedWebSite.cs
@@ -0,0 +1,17 @@
+﻿namespace Bounce.Framework
+{
+    public class Iis6StoppedWebSite : Iis6Task
+    {
+        [Dependency]
+        public Future<string> Name;
+
+        public override void Build()
+        {
+            IisWebSite webSite = Iis.TryGetWebSiteByServerComment(Name.Value);
+            if (webSite != null)
+            {
+                webSite.Stop();
+            }
+        }
+    }
+}
