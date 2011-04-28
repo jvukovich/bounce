a0392b3100935f4877104d77508f5c67168813dd
Tim Macfarlane <timmacfarlane@gmail.com>
2011-04-28 13:42:17 +0100

    using builder for staged deploy targets, more flexible

---
+++ Bounce.Framework/StagedDeployTargetBuilder.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace Bounce.Framework
+{
+    public class StagedDeployTargetBuilder
+    {
+        public IDictionary<string, ITask> Targets { get; private set; }
+        public Task<IEnumerable<DeployMachine>> MachineConfigurations { get; private set; }
+        public IRemoteBounceFactory RemoteBounceFactory { get; private set; }
+        public Parameter<string> Stage { get; private set; }
+
+        public StagedDeployTargetBuilder()
+        {
+            Targets = new Dictionary<string, ITask>();
+        }
+
+        public StagedDeployTargetBuilder ForMachines(Task<IEnumerable<DeployMachine>> machineConfigurations)
+        {
+            StagedDeployTargetBuilder builder = Clone();
+            builder.MachineConfigurations = machineConfigurations;
+            return builder;
+        }
+
+        public StagedDeployTargetBuilder WithTargets(IDictionary<string, ITask> targets)
+        {
+            StagedDeployTargetBuilder builder = Clone();
+            builder.Targets = targets;
+            return builder;
+        }
+
+        public StagedDeployTargetBuilder WithRemoteBounceFactory(IRemoteBounceFactory remoteBounceFactory)
+        {
+            StagedDeployTargetBuilder builder = Clone();
+            builder.RemoteBounceFactory = remoteBounceFactory;
+            return builder;
+        }
+
+        public StagedDeployTargetBuilder WithStage(Parameter<string> stage)
+        {
+            StagedDeployTargetBuilder builder = Clone();
+            builder.Stage = stage;
+            return builder;
+        }
+
+        private StagedDeployTargetBuilder Clone()
+        {
+            return new StagedDeployTargetBuilder
+                       {
+                           Targets = Targets,
+                           MachineConfigurations = MachineConfigurations,
+                           RemoteBounceFactory = RemoteBounceFactory,
+                           Stage = Stage
+                       };
+        }
+
+        public StagedDeployTarget CreateTarget(string name)
+        {
+            var target = new StagedDeployTarget(name, Stage, MachineConfigurations, RemoteBounceFactory);
+            Targets[name] = target;
+            return target;
+        }
+    }
+}
