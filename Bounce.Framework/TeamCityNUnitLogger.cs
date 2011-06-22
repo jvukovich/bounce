4b121ef0bf931e0d5a2b7b298337ff56c225e7fe
Matt Richardson <matt@redgumtech.com.au>
2011-06-22 08:32:09 +0100

    Output inconclusive test results for teamcity versions less than 6

--- Bounce.Framework/TeamCityNUnitLogger.cs
+++ Bounce.Framework/TeamCityNUnitLogger.cs
@@ -7,23 +7,16 @@ namespace Bounce.Framework {
         private readonly TextWriter Output;
         private readonly ICommandLog Log;
         private readonly TeamCityFormatter TeamCityFormatter;
-        private readonly Regex SummaryRegex;
-        private readonly bool InconclusiveTestCountRequired;
-
+        
         public TeamCityNUnitLogger(string args, TextWriter output, ICommandLog log) {
             Output = output;
             Log = log;
             CommandArgumentsForLogging = args;
             TeamCityFormatter = new TeamCityFormatter();
-
-            SummaryRegex = new Regex(@"Tests run: (\d.*), Errors: (\d.*), Failures: (\d.*), Inconclusive: (\d.*), Time: (\d.*) seconds");
-            InconclusiveTestCountRequired = DetermineIfInconclusiveTestCountRequired();
         }
 
         public void CommandOutput(string output)
         {
-            OutputInconclusiveTestCount(output);
-
             Log.CommandOutput(output);
         }
 
@@ -38,33 +31,5 @@ namespace Bounce.Framework {
         }
 
         public string CommandArgumentsForLogging { get; private set; }
-
-        private static bool DetermineIfInconclusiveTestCountRequired()
-        {
-            var version = Environment.GetEnvironmentVariable("TEAMCITY_VERSION");
-            if (version == null) return false;
-            
-            var regex = new Regex(@"([0-9].*)\.([0-9].*)\.([0-9].*) \(build \d.*\)");
-            var match = regex.Match(version);
-            
-            if (match.Success)
-            {
-                return (int.Parse(match.Groups[1].Value) < 6);
-            }
-            return true;
-        }
-
-        private void OutputInconclusiveTestCount(string output)
-        {
-            if (!InconclusiveTestCountRequired) return;
-            if (output == null) return;
-            var match = SummaryRegex.Match(output);
-            
-            if (match.Success && int.Parse(match.Groups[4].Value) > 0)
-            {
-                    Output.WriteLine(TeamCityFormatter.FormatTeamCityMessageWithFields("buildStatus", "text",
-                        "{build.status.text}, inconclusive: " + match.Groups[4].Value));
-            }
-        }
     }
 }
