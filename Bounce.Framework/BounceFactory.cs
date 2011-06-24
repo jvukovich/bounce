0f84525ce1e96465da010a738fb48c57a6beac28
alexander.beletsky <alexander.beletsky@gmail.com>
2011-06-24 14:40:20 +0300

    added CommandFactory and BounceFactory to get those object outside the framework;
    corrected TargetRetriever to recieve just object, instead of MethodInfo;

---
+++ Bounce.Framework/BounceFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bounce.Framework
+{
+    public class BounceFactory
+    {
+        public static IBounce CreateBounce()
+        {
+            return new Bounce();
+        }
+    }
+}
--- Bounce.Framework/ITargetsRetriever.cs
+++ Bounce.Framework/ITargetsRetriever.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace Bounce.Framework {
     public interface ITargetsRetriever {
         IDictionary<string, ITask> GetTargetsFromAssembly(MethodInfo getTargetsMethod, IParameters parameters);
+        IDictionary<string, ITask> GetTargetsFromObject(object targets);
     }
 }
