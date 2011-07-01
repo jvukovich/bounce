52a3484a4bcd488423031376abcc4deeaae25190
Matt Richardson <matt@redgumtech.com.au>
2011-07-01 16:48:43 +0100

    Split out 3.5 and 4.0 builds and tests

--- Build/Build.cs
+++ Build/Build.cs
@@ -12,21 +12,28 @@ namespace Build
         public static object Targets(IParameters parameters)
         {
             var v4 = new VisualStudioSolution {SolutionPath = @"Bounce.sln", Configuration = "Debug"};
-            var v35 = new VisualStudioSolution {SolutionPath = @"Bounce.sln", Configuration = "Debug_3_5"};
-            var tests = new NUnitTests
+            var v35 = new VisualStudioSolution {SolutionPath = @"Bounce.sln", Configuration = "Debug_3_5" };
+
+            var v4Tests = new NUnitTests
             {
                 DllPaths = v4.Projects.Where(p => p.Name.EndsWith("Tests")).Select(p => p.OutputFile),
+                NUnitConsolePath = @"References\NUnit\nunit-console.exe"
+            };
+            
+            var v35Tests = new NUnitTests
+            {
+                DllPaths = v35.Projects.Where(p => p.Name.EndsWith("Tests")).Select(p => p.OutputFile),
                 NUnitConsolePath = @"References\NUnit\nunit-console.exe"
             };
 
             Task<IEnumerable<string>> dests = new [] {"sdf"};
             dests.SelectTasks(dest => new Copy {ToPath = dest});
 
-            var nugetExe = @"References\NuGet\NuGet.exe";
+            const string nugetExe = @"References\NuGet\NuGet.exe";
             var nugetPackage = new NuGetPackage
             {
                 NuGetExePath = nugetExe,
-                Spec = v4.Projects["Bounce.Framework"].ProjectFile.WithDependencyOn(tests, v35),
+                Spec = v4.Projects["Bounce.Framework"].ProjectFile.WithDependencyOn(v4Tests, v35Tests, v35),
             };
 
             var nugetPush = new NuGetPush
@@ -38,8 +45,12 @@ namespace Build
 
             return new
             {
+                Net4Binaries = v4,
+                Net35Binaries = v35,
+                Net4Tests = v4Tests,
+                Net35Tests = v35Tests,
                 Binaries = new All(v4, v35),
-                Tests = tests,
+                Tests = new All(v4Tests, v35Tests),
                 NuGet = nugetPush,
                 NuGetPackage = nugetPackage,
             };
