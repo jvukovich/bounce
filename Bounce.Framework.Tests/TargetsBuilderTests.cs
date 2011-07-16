d5d4bce1d415fe5c1d37a26d41fcfd3b5e6174f2
alexander.beletsky <alexander.beletsky@gmail.com>
2011-07-16 12:27:31 +0300

    Fixed NullReferenceException in case if task in not initialized;

---
+++ Bounce.Framework.Tests/TargetsBuilderTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+
+namespace Bounce.Framework.Tests {
+    [TestFixture]
+    public class TargetsBuilderTests {
+        [Test]
+        public void BuildTargetsShouldSkipNullTargets() {
+            // arrange
+            var bounceMock = new Mock<ITargetBuilderBounce>();
+            var commandMock = new Mock<IBounceCommand>();
+
+
+            bounceMock.Setup(_ => _.TaskScope(It.IsAny<ITask>(), commandMock.Object, It.IsAny<string>())).Returns(new Mock<ITaskScope>().Object);
+            bounceMock.Setup(_ => _.Invoke(commandMock.Object, It.IsAny<ITask>())).Callback((IBounceCommand c, ITask t) => {
+                if (t == null) {
+                    throw new NullReferenceException();
+                }
+            });
+
+            var targets = new List<Target> { new Target { Name = "OK", Task = new Mock<ITask>().Object }, new Target { Name = "Fail", Task = null } };
+            var targetsBuilder = new TargetsBuilder();
+
+            // act / assert (no exception)
+            targetsBuilder.BuildTargets(bounceMock.Object, targets, commandMock.Object);
+        }
+    }
+}
