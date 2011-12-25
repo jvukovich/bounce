09b7069c8adb13f2f34c1e3a7006fe85e1cf577b
alexander.beletsky <alexander.beletsky@gmail.com>
2011-12-25 12:19:09 +0200

    added Iis7 Start/Stop tasks

---
+++ Bounce.Framework/Iis7StoppedSite.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.Web.Administration;
+
+namespace Bounce.Framework
+{
+    public class Iis7StoppedSite : Task
+    {
+        [Dependency]
+        public Task<string> Name;
+        [Dependency]
+        public Task<TimeSpan> Wait;
+
+        public Iis7StoppedSite()
+        {
+            Wait = TimeSpan.FromMilliseconds(0);
+        }
+
+        public override void Build()
+        {
+            var iisServer = new ServerManager();
+            var site = iisServer.Sites[Name.Value];
+
+            if (site != null)
+            {
+                site.Stop();
+                Thread.Sleep(Wait.Value);
+            }
+        }
+    }
+}
