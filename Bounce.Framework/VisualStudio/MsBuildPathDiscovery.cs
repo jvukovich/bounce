1211c55f02aef5357177bcf29011778f90e26525
David Whitney <david@davidwhitney.co.uk>
2016-08-28 01:23:42 +0100

    Better formatting

--- Bounce.Framework/VisualStudio/MsBuildPathDiscovery.cs
+++ Bounce.Framework/VisualStudio/MsBuildPathDiscovery.cs
@@ -7,15 +7,18 @@ namespace Bounce.Framework.VisualStudio {
     class MsBuildPathDiscovery {
 
         public Func<string, bool> FileExitsCheck { get; set; }
-        public readonly List<string> MsBuildLocations = new List<string> {
-            Environment.ExpandEnvironmentVariables(@"%MSBuild%"),
-            Environment.ExpandEnvironmentVariables(@"%ProgramFiles%\MSBuild\14.0\Bin\msbuild.exe"),
-            Environment.ExpandEnvironmentVariables(@"%ProgramFiles%\MSBuild\12.0\Bin\msbuild.exe"),
-            Environment.ExpandEnvironmentVariables(@"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe")
-        };
+        public readonly List<string> MsBuildLocations;
 
         public MsBuildPathDiscovery(Func<string, bool> fileExitsCheck = null) {
             FileExitsCheck = fileExitsCheck ?? File.Exists;
+
+            MsBuildLocations = new List<string> {
+                Environment.ExpandEnvironmentVariables(@"%MSBuild%"),
+                Environment.ExpandEnvironmentVariables(@"%ProgramFiles%\MSBuild\14.0\Bin\msbuild.exe"),
+                Environment.ExpandEnvironmentVariables(@"%ProgramFiles%\MSBuild\12.0\Bin\msbuild.exe"),
+                Environment.ExpandEnvironmentVariables(@"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe")
+            };
+
             if (MsBuildLocations.First() == "%MSBuild%") {
                 MsBuildLocations.RemoveAt(0);
             }
