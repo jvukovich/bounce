72fe0dde1a73f6d27f430992fb41bcb46cc94810
Jonathan Vukovich <jon.vukovich@gmail.com>
2019-03-31 19:00:22 -0600

    Fixing filter after project renamed.

---
+++ Bounce/OptionsAndArguments.cs
@@ -0,0 +1,9 @@
+﻿namespace Bounce
+{
+    public class OptionsAndArguments
+    {
+        public string[] RemainingArguments { get; set; }
+        public string BounceDirectory { get; set; }
+        public string WorkingDirectory { get; set; }
+    }
+}
---
+++ Bounce/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Bounce.Framework;
+
+namespace Bounce
+{
+    internal static class Program
+    {
+        private static int Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1)
+                {
+                    if (args.Contains("--help") || args.Contains("-h"))
+                    {
+                        UsageHelp.WriteUsage();
+                        return 0;
+                    }
+
+                    if (args.Contains("--version") || args.Contains("-v"))
+                    {
+                        var version =
+                            Assembly.GetEntryAssembly()
+                                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                .InformationalVersion;
+
+                        Console.WriteLine(version);
+
+                        return 0;
+                    }
+                }
+
+                BounceAssemblyRunner.Run(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return 70;
+            }
+
+            return 0;
+        }
+    }
+}
